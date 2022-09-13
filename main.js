626842b7fb1794c533c6bace4a0268cca67e2047
brymedia <56924275+yungbrym@users.noreply.github.com>
2022-09-13 18:49:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
 
 
-    let tablinks = document.getElementsByClassName("tab-links");
-    let tabcontents = document.getElementsByClassName("tab-contents");
-
-    function opentab(tabname){
-        for(tablink of tablinks){
-            tablink.classList.remove("active-link");
-        }
-        for(tabcontent of tabcontents){
-            tabcontent.classList.remove("active-tab");
-        }
-        event.currentTarget.classList.add("active-link");
-        document.getElementById(tabname).classList.add("active-tab");
-    }
-
-
-
-
-
     var sidemeu = document.getElementById("sidemenu");
 
     function openmenu(){
@@ -29,20 +11,14 @@
 
 
 
-    const scriptURL = 'https://script.google.com/macros/library/d/104MbCmA2t35aTu4DIGll_ijFDJCv2X7V7oaU8wjrX0WecpZSGKyhO2l8/1' // add your own app script link here
-    const form = document.forms['submit-to-google-sheet']
-    const msg = document.getElementById("msg")
-  
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function(){
-                msg.innerHTML = ""
-            },5000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
-    })
+    const scriptURL =
+    "https://script.google.com/macros/s/AKfycbxJjjhEMKyq6IRrrX_gc-9ElUlU1tP9ND_AgWQuRIazOQDcBdyMje8YP6deL5quGkZE/exec"
+    const form = document.forms["submit-to-google-sheet"];
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      fetch(scriptURL, { method: "POST", body: new FormData(form) })
+        .then((response) => console.log("Success!", response))
+        .catch((error) => console.error("Error!", error.message));
+    });
   
